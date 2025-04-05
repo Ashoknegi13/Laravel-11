f2fed950c644bda1ede435c6698e90b945cc695f
AshokNegi13 <negiashok1540@gmail.com>
2025-04-05 07:57:23 +0530

    .... completed all Seeder topic or Artisan cmd releted to seeder and how to insert real and fake data using seeder and also how to insert json file data into database and one last and important topic different between insert() and create() ....

--- Database-Seeder-Project/database/seeders/StudentSeeder.php
+++ Database-Seeder-Project/database/seeders/StudentSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\student;
+use Illuminate\Support\facades\file;
 
 class StudentSeeder extends Seeder
 {
@@ -12,33 +13,53 @@ class StudentSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {   
-        $students = collect([
-            [
-                'name'=>'rahul negi',
-                'email'=>'rahul@gmail.com'
-            ],
-            [
-                'name'=>'Saurabh bisht',
-                'email'=>'saurabhbisht@gmail.com'
-            ],
-            [
-                'name'=>'vipin kaprawan',
-                'email'=>'vipi@gmail.com'
-            ],
-            [
-                'name'=>'Ashok negi',
-                'email'=>'negiashok132@gmail.com'
-            ]
-]);
-
-    $students->each(function($stu_data){
-        student::insert($stu_data);
-    });
+    {
+        // .. this is how to insert multiple fake data
+        for($i=0;$i<10;$i++){
+            student::create([
+                "name"=>fake()->name(),
+                "email"=>fake()->unique()->email()
+            ]);
+        }
 
 
+   //... this is how to insert single fake data using fake()
         // student::create([
-        //     'name'=>'ashok annnu negi',
+        //     'name'=>fake()->name(),
+        //     'email'=>fake()->unique()->email()
+        // ]);
+
+
+
+
+        
+// ...it is json file data and we use only create because of timestamp 
+
+    //     $json = File::get(path:"database/json/student.json");
+    //     $students = collect(json_decode($json));
+
+    // $students->each(function($stu){
+    //     student::create([
+    //         'name'=>$stu->name,
+    //         'email'=>$stu->email
+    //     ]);
+    // });
+    
+
+
+
+
+//.... it is simple data using collection / collect ()
+    // $students->each(function($stu_data){
+    //     student::insert($stu_data);
+    // });
+
+
+
+    // ........it is simple data without json file
+
+        // student::insert([
+        //     'name'=>'ashok negiiiii',
         //     'email'=>'negiashok132@gmail.com'
         // ]);
     }
