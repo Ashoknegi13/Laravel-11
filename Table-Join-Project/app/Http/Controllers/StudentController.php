70292b20cd56d56e9e152b89e1dc685d86ebf6d2
AshokNegi13 <negiashok1540@gmail.com>
2025-04-21 08:37:31 +0530

    ..

--- Table-Join-Project/app/Http/Controllers/StudentController.php
+++ Table-Join-Project/app/Http/Controllers/StudentController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use  Illuminate\Support\Facades\DB;
+
 
 class StudentController extends Controller
 {
-    //
+     public function showStudents(){
+        $students = DB::table('students')
+                     // ->select(DB::raw('count(*) as Total_students'))  return a total data 
+                   
+                     ->select('students.*','cities.city_name')
+                    ->join('cities','students.city','=','cities.id') 
+                    ->get();    
+                    
+                    // ->where('students.name','LIKE','a%')
+                    // ->count();     don't use get() when we use count()
+                    // ->where('cities.city_name','=','srinager')
+                    // ->where('students.email','=','negiashok132@gmail.com')
+
+                    
+
+        return $students;
+
+        // return view('welcome',compact('students'));
+     }
 }
--- Table-Join-Project/app/Models/citie.php
+++ Table-Join-Project/app/Models/citie.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class citie extends Model
 {
-    //
+    public $timestamps = false;
 }
