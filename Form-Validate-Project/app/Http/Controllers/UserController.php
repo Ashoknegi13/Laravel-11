7a848066dd1f4b03db9eca88a6ff117f60c3cc4f
AshokNegi13 <negiashok1540@gmail.com>
2025-05-10 08:48:09 +0530

    Completed Form Validation Topics..

--- Form-Validate-Project/app/Http/Controllers/UserController.php
+++ Form-Validate-Project/app/Http/Controllers/UserController.php
@@ -11,8 +11,20 @@ class UserController extends Controller
             'username'=>'required',
             'userpass'=>'required | alpha_num | min:6',
             'useremail'=>'required | email',
-            'userage'=>'required | numeric',
+            'userage'=>'required | numeric | min:18',
             'usercity'=>'required'
+        ],[
+            'username.required'=>'User Name Required',
+            'userpass.required'=>'Password Is Required',
+            'userpass.alpha_num'=>'only number and Alphabate allowed',
+            'userpass.min'=>'enter min 6 digit',
+            'userage.min'=>'min age required 18 ',
+            'useremail.required'=>'Email Address Must Must  Required',
+            'useremail.email'=>'Enter correct email format',
+            'userage.required'=> 'age is required',
+            'userage.numeric'=>'Enter only numerci value',
+            'usercity.required'=>'city is required'
+
         ]);   
         return $req->all();
     }
