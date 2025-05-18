e0d744e4ed6751104d2a18e636b83b5eb677bf12
AshokNegi13 <negiashok1540@gmail.com>
2025-05-18 20:31:02 +0530

    ...

--- laravel-Resource-Project/routes/web.php
+++ laravel-Resource-Project/routes/web.php
@@ -7,4 +7,20 @@ use App\Http\Controllers\UserController;
 // });
 
 
-Route::resource('users',UserController::class);
+// Route::resource('users',UserController::class);
+
+// Route::resource('users', UserController::class)->only([
+//     'index','create'
+// ]);
+
+
+// Route::resource('users', UserController::class)->except([
+//     'destroy'
+// ]);
+
+
+Route::resource('users', UserController::class)->names([
+    'create' => 'users.add',
+    'index'=> 'users.home',
+    'destroy'=>'users.delete'
+]);
