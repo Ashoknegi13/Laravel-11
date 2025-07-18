7cb451411bc1ff21c24765c1728ca510d057bce8
AshokNegi13 <negiashok1540@gmail.com>
2025-07-18 10:20:19 +0530

    ...

--- One-To-Many-Relationship-Project/app/Http/Controllers/BookController.php
+++ One-To-Many-Relationship-Project/app/Http/Controllers/BookController.php
@@ -8,16 +8,16 @@ use App\Models\Book;
 class BookController extends Controller
 {
     public function showBook(){
-        $books = Book::with('student')->find(2);
-         return $books;
+        $books = Book::with('student')->get();
+        //  return $books;  
 
-        // foreach($books as $book){
-        //     echo "<div style='border:1px solid black;margin-bottom:10px '>
-        //                 <h1>$book->book</h1>
-        //                 <span>$book->book_details</span>
-        //                 <h3>{$book->student->name}</h3>
-        //                 <h3>{$book->student->Age}</h3>
-        //          </div>";
-        // }
+        foreach($books as $book){
+            echo "<div style='border:1px solid black;margin-bottom:10px '>
+                        <h1>$book->book</h1>
+                        <span>$book->book_details</span>
+                        <h3>{$book->student->name}</h3>
+                        <h3>{$book->student->Age}</h3>
+                 </div>";
+        }
     }
 }
