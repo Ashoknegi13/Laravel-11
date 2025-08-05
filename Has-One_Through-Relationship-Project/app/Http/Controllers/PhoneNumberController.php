e537a85b107d7d2d7d928618d95bcc2ba7bcc591
AshokNegi13 <negiashok1540@gmail.com>
2025-08-05 07:49:34 +0530

    ...

---
+++ Has-One_Through-Relationship-Project/app/Http/Controllers/PhoneNumberController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Phone_number;
+use Illuminate\Http\Request;
+
+class PhoneNumberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Phone_number $phone_number)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Phone_number $phone_number)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Phone_number $phone_number)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Phone_number $phone_number)
+    {
+        //
+    }
+}
---
+++ Has-One_Through-Relationship-Project/database/seeders/NumberSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Phone_number;
+use Illuminate\Support\Facades\File;
+
+class NumberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $json = File::get(path:'database/json/number.json');
+        $num = collect(json_decode($json));
+
+        $num->each(function($data){
+            Phone_number::Create([
+                'numbers'=>$data->number,
+                'company_id'=>$data->c_id
+            ]);
+        });
+    }
+}
